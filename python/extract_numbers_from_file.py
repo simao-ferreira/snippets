6c4072fda7f850c860f3ec4579aac3e81f9f20e4
Simao Ferreira <simao.ferreira@tuta.io>
2020-08-04 21:21:08 +0200

    SNI: Update python extraction snippet

--- python/extract_numbers_from_file.py
+++ python/extract_numbers_from_file.py
@@ -1,14 +1,17 @@
+# Extract id's from string in a text file
+# Version 0.2
 import re
 
 lines = []
-value = 'nr:'
+value = 'id: '
 pattern = re.compile(value, re.IGNORECASE)          # Compile a case-insensitive regex
 
 with open ('lorem.txt', 'rt') as my_file:           # Open file
     for line in my_file:
         if pattern.search(line) != None:            # If a match is found 
-            res = line.split(value)[-1]             # Split the line by string
-            lines.append(re.findall('\d+', res)[0]) # Find all numbers in substr and append the first
+            parted = re.findall(value + '\d+', line)        # Find all value + number (to ignore other numbers) 
+            for part in parted:
+                lines.append(re.findall('\d+', part)[0])    # Find all numbers in substr and add to list
 
 lines = list(dict.fromkeys(lines))                  # Remove duplicates
 for number in lines:                                # Iterate over the list of tuples
